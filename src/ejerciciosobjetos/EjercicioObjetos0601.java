adad162c1a143ffd09acac560fde6e0b90a5135e
Jose Doval <dovaldam108@gmail.com>
2019-10-24 17:37:08 +0200

    #1

---
+++ src/ejerciciosobjetos/EjercicioObjetos0601.java
@@ -0,0 +1,17 @@
+package ejerciciosobjetos;
+import java.util.Scanner;
+public class EjercicioObjetos0601 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        
+        MovilPrepago movil = new MovilPrepago(605308712, 0.35f, 0.05f, 0.10f, 10);
+        
+        // hacer una llamada
+        llamada = movil.consultarSaldo(500f);
+        // hacer una navegacion
+        // hacer una recarga
+        // consultar sadldo
+    }
+    
+    
+}
